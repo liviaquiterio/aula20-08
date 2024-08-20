02806039dc1d0102606ae643e84814c3cc0e9775
liviaquiterio <127116102+liviaquiterio@users.noreply.github.com>
2024-08-20 09:30:49 -0300

    Add files via upload

---
+++ inserircomponente.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inserir Componente</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    <h3 class="text-center text-sucess">INSERIR COMPONENTE</h3>
+    <?php
+     $curso = $_POST["curso"];
+     $componente = $_POST["componente"];
+     
+    ?>
+</body>
+</html>
